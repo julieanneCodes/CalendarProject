f82f19cc237552e4ea3c6bc796a0f8b10dcc84b3
julieanneCodes <j.o.131502@gmail.com>
2020-11-03 22:13:00 +0100

    Auto login after succesfull registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,17 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\UserType;
-use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\User;
+use App\Security\LoginFormAuthAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register", name="user_register")
      */
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    public function register(LoginFormAuthAuthenticator $authenticator, GuardAuthenticatorHandler $guard, Request $request, UserPasswordEncoderInterface $passwordEncoder)
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
@@ -30,7 +31,13 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('user_index');
+            $guard->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main'
+            );
+            return $this->redirectToRoute('user_config_new');
         }
         return $this->render('registration/new.html.twig', 
             array('form' => $form->createView())
