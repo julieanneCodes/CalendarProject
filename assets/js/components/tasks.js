a73867a4f1be928c3b796bf4047b2b43b26f5d17
julieanneCodes <j.o.131502@gmail.com>
2020-12-13 23:11:58 +0100

    Tasks component added

---
+++ assets/js/components/tasks.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from 'lit-element';
+import { dateFormatter } from '../utils/functions';
+class Tasks extends LitElement {
+  static get styles() {
+    return css`
+      .tasksWrapper {
+        margin-top: 87px;
+      }
+      .tsk-item {
+        background-color: #5F8FB4;
+        color: whitesmoke;
+        margin-bottom: 5px;
+        padding: 10px;
+      }
+      .tsk-item div {
+        font-size: 20px;
+      }
+
+    `;
+  }
+
+  static get properties() {
+    return {
+      data: { type: Array }
+    }
+  }
+
+  constructor() {
+    super();
+    this.data= [];
+  }
+
+  render() {
+    return html`
+      <div class="tasksWrapper">
+        ${this.data.map(item => html`
+          <div class="tsk-item">
+            <div class="dateWrap">${dateFormatter(item.day).default}</div>
+            <div class="nameWrap">${item.taskname}</div>
+          </div>
+          `)}
+      </div>
+    `;
+  }
+
+}
+customElements.define('task-component', Tasks)
--- src/Controller/SingleController.php
+++ src/Controller/SingleController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Calendar;
 use App\Repository\CalendarRepository;
+use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Security;
@@ -18,7 +19,7 @@ class SingleController extends AbstractController
         $this->security= $security;
     }
     /**
-     * @Route("/single/calendar", name="single-calendar")
+     * @Route("/single/calendar", name="single_calendar")
      */
     public function index(CalendarRepository $calendarRepository, SerializerInterface $serializer): Response
     {
@@ -29,7 +30,24 @@ class SingleController extends AbstractController
         return $this->render('single/index.html.twig', [
             'data' => $jsonData,
             'user' => $user,
+            'route' => 'single_tasks',
+            'routeName' => 'view tasks'
         ]);
     }
+    /**
+     * @Route("/single/tasks", name="single_tasks")
+     */
+    public function tasksIndex(TaskRepository $taskRepo, SerializerInterface $serializer): Response
+    {
+        $user = $this->security->getUser();
+        $id = $user->getId();
+        $jsonData = $serializer->serialize($taskRepo->findAllById($id), 'json', ['groups' => 'tasks_data']);
 
+        return $this->render('single/index.html.twig', [
+            'data' => $jsonData,
+            'user' => $user,
+            'route' => 'single_calendar',
+            'routeName' => 'view events'
+        ]);
+    }
 }
