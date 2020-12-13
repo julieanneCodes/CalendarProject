7ebeb212d94f9188c37fa59963af53243d117fdf
julieanneCodes <j.o.131502@gmail.com>
2020-12-13 23:13:37 +0100

    Styles added

--- assets/js/css/calendar-styles.js
+++ assets/js/css/calendar-styles.js
@@ -3,7 +3,7 @@ export const calendarStyles = css`
   .calendarWrap {
   display: flex;
   flex-wrap: wrap;
-  padding: 30px;
+  margin-top: 40px;
   }
   .namesWrap {
     border: none;
@@ -16,5 +16,33 @@ export const calendarStyles = css`
   margin: 5px;
   height: 100px;
   border: 1px solid black;
+  }
+  .mth-wrp {
+    font-size: 20px;
+    color: #256BA2;
+  }
+  .stp-container {
+    position: relative;
+  }
+  .stepperWrap {
+    position: absolute;
+    right: 5px;
+    display: inline-flex;
+    align-items: center;
+  }
+  .btn-s {
+    border: none;
+    background-color: #256BA2;
+    color: whitesmoke;
+    border-radius: 5px;
+    height: 35px;
+    width: 50px;
+  }
+  .btn-sm {
+    height: 35px;
+    background-color: #5F8FB4;
+    color: whitesmoke;
+    border: none;
+    border-radius: 5px;
   }
 `;
---
+++ assets/js/css/double-styles.js
@@ -0,0 +1,10 @@
+import { css } from 'lit-element';
+export const doubleStyles = css`
+  .wholeWrapper {
+    display: flex;
+    padding-left: 15px;
+  }
+  .tasksWrapper {
+    width: 25%;
+  }
+`;
