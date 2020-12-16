97c7c7592141ff8b9c8a0901f76b4debaf3ab777
julieanneCodes <j.o.131502@gmail.com>
2020-12-16 03:57:37 +0100

    consts added

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -1,2 +1,3 @@
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 export const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+export const completeDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
--- assets/js/utils/functions.js
+++ assets/js/utils/functions.js
@@ -1,4 +1,4 @@
-import { months } from './constants';
+import { months, completeDays } from './constants';
 
 export const dateFormatter = (date_) => {
   const date = new Date(date_);
@@ -7,7 +7,7 @@ export const dateFormatter = (date_) => {
   const monthName = months[date.getMonth()];
   const year = date.getFullYear();
   const month = date.getMonth();
-  //const weekDayName = days[date.getDay()];
+  const weekDayName = completeDays[date.getDay()];
   const hour = date.getHours();
   const minute = date.getMinutes();
 
@@ -15,9 +15,9 @@ export const dateFormatter = (date_) => {
     default: monthName + ' ' + monthDay + ', ' + year,
     databased: new Date(year, month, monthDay).getTime(),
     short: monthName.slice(0, 3) + ' ' + monthDay,
-    //day: weekDayName,
+    display: weekDayName + ', ' + monthDay + ' of ' + monthName + ' ' + year,
     monthName: monthName,
-    hour: (hour < 10 ? '0' + hour : hour) + ':' + (minute < 10 ? '0' + minute : minute),
+    hour: (hour < 10 ? '0' + hour : hour) + ':' + (minute < 10 ? '0' + minute : minute) + ' h',
     input: `${date.getFullYear()}-${date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}-${
       date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
     }`,
