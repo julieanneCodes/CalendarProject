ce8d1eec7580db62ae5234760f3a2243df585344
julieanneCodes <j.o.131502@gmail.com>
2021-01-11 03:23:56 +0100

    const fixed and added new ones to functions

--- assets/js/utils/constants.js
+++ assets/js/utils/constants.js
@@ -1,3 +1,3 @@
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-export const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-export const completeDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+export const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+export const completeDays = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
--- assets/js/utils/functions.js
+++ assets/js/utils/functions.js
@@ -1,4 +1,4 @@
-import { months, completeDays } from './constants';
+import { months, completeDays, days } from './constants';
 
 export const dateFormatter = (date_) => {
   if (typeof date_ == "undefined" ) {
@@ -11,15 +11,18 @@ export const dateFormatter = (date_) => {
   const year = date.getFullYear();
   const month = date.getMonth();
   const weekDayName = completeDays[date.getDay()];
+  const weekDayShort = days[date.getDay()];
   const hour = date.getHours();
   const minute = date.getMinutes();
 
   return {
     default: monthName + ' ' + monthDay + ', ' + year,
+    weekDay: weekDayName + ' ' + monthDay,
     databased: new Date(year, month, monthDay).getTime(),
     display: weekDayName + ', ' + monthDay + ' of ' + monthName + ' ' + year,
     monthName: monthName,
-    hour: (hour < 10 ? '0' + hour : hour) + ':' + (minute < 10 ? '0' + minute : minute) + ' h -',
+    monthYear: monthName + ' ' + year,
+    hour: (hour < 10 ? '0' + hour : hour) + ':' + (minute < 10 ? '0' + minute : minute) + ' h',
     input: `${date.getFullYear()}-${date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}-${
       date.getDate() < 10 ? `0${date.getDate()}` : date.getDate()
     }`,
